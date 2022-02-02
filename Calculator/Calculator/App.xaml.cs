266088f9072a5ec800a3df2ead8fd5deb08f66e8
mahan <mahan@MSI>
2022-02-02 17:16:54 -0500

    Add project files.

---
+++ Calculator/Calculator/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Calculator
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            //MainPage = new CalculatorPage();
+            MainPage = new Buttons();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Calculator/Calculator/Buttons.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Calculator
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Buttons : ContentPage
+    {
+        public Buttons()
+        {
+            InitializeComponent();
+
+            buttonTest.Text = "Changed Text By Code";
+            buttonTest.Clicked += (object sender, EventArgs e) =>
+            {
+                if (labelTest.TextColor != Color.Red)
+                {
+                    labelTest.TextColor = Color.Red;
+                } else
+                {
+                    labelTest.TextColor = Color.Green;
+                }
+                
+            };
+        }
+
+        private void buttonBorder_Clicked(object sender, EventArgs e)
+        {
+            if (labelTest.TextColor != Color.White)
+            {
+                labelTest.TextColor = Color.White;
+            }
+            else
+            {
+                labelTest.TextColor = Color.Black;
+            }
+        }
+    }
+}
